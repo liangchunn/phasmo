02743bcd99b1d8861e43cfd8ca228eef64d7a7d8
Liang Chun Wong <liang-chun.wong@trivago.com>
2021-03-13 21:15:43 +0100

    feat(evidence-selector): don't highlight selected evidence when in eliminate mode

--- src/components/EvidenceSelector/EvidenceSelector.tsx
+++ src/components/EvidenceSelector/EvidenceSelector.tsx
@@ -41,7 +41,9 @@ export default function EvidenceSelector({
                   evidenceKey
                 )))
           }
-          isSelected={selectedEvidence.includes(evidenceKey)}
+          isSelected={
+            selectedEvidence.includes(evidenceKey) && !isInEliminateMode
+          }
         />
       ))}
     </div>
